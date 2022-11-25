a086b96c360da2eabc5baf8b09563992eb68bce3
Advik <advik.b@gmail.com>
2022-11-26 00:13:21 +0530

    Update base.py

--- curseforge/base.py
+++ curseforge/base.py
@@ -1,7 +1,7 @@
 from requests import get
 from dataclasses import dataclass
 from typing import Generator
-from classes import CurseGame, CurseGameAssets
+from classes import Game, GameAssets, Category
 
 BASE_URL = "https://api.curseforge.com"
 
@@ -11,55 +11,58 @@ class CurseClient:
     api_key: str
     version: str = "v1"
 
-    def fetch(self, url: str):
+    def fetch(self, url: str, params: dict = None):
         return get(
             f"{BASE_URL}/{self.version}/{url}",
             headers={
                 "X-API-Key": self.api_key,
                 "Accept": "application/json"
-            }
+            },
+            params=params
         ).json()["data"]
 
     def game(self, game_id: int):
         _game = self.fetch(f"games/{game_id}")
-        return CurseGame(
+        return Game(
             id=_game["id"],
             name=_game["name"],
             slug=_game["slug"],
             url=_game["url"],
-            assets=CurseGameAssets(*_game["assets"]),
+            assets=GameAssets(*_game["assets"]),
             status=_game["status"],
             api_status=_game["api_status"],
             date_modified=_game["date_modified"],
         )
 
-    def games(self) -> tuple[CurseGame]:
-        """Returns a tuple of CurseGame objects"""
-        # We are using a tuple here because we don't want to allow the user to modify the list
-        return tuple(
-                CurseGame(
-                    id=game["id"],
-                    name=game["name"],
-                    slug=game["slug"],
-                    url=game["url"],
-                    assets=CurseGameAssets(*game["assets"]),
-                    status=game["status"],
-                    api_status=game["api_status"],
-                    date_modified=game["date_modified"],
-                )
-                for game in self.fetch("games")
-        )
-
-    def games_iter(self) -> Generator[CurseGame, ...]:
+    def games(self) -> Generator[Game, ...]:
         """Returns a generator of CurseGame objects to iterate over live"""
         for game in self.fetch("games"):
-            yield CurseGame(
+            yield Game(
                 id=game["id"],
                 name=game["name"],
                 slug=game["slug"],
                 url=game["url"],
-                assets=CurseGameAssets(*game["assets"]),
+                assets=GameAssets(*game["assets"]),
                 status=game["status"],
                 api_status=game["api_status"],
                 date_modified=game["date_modified"],
+            )
+
+    def game_versions(self, game_id: int):
+        return self.fetch(f"game/{game_id}/versions")
+
+    def categories(self, game_id: int) -> Generator[Category, ...]:
+        for category in self.fetch("categories", {"gameId": game_id}):
+            yield Category(
+                id=category["id"],
+                game_id=category["gameId"],
+                name=category["name"],
+                slug=category["slug"],
+                url=category["url"],
+                icon_url=category["iconUrl"],
+                date_modified=category["dateModified"],
+                is_Class=category["isClass"],
+                class_id=category["classId"],
+                parentCategory_id=category["parentCategoryId"],
+                displayIndex=category["displayIndex"],
             )
