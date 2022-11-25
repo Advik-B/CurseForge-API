03e7a45b9a28fe40e3738a747444c41cff36e97b
Advik <advik.b@gmail.com>
2022-11-25 23:28:40 +0530

    Update base.py

--- curseforge/base.py
+++ curseforge/base.py
@@ -18,6 +18,20 @@ class CurseClient:
             }
         ).json()["data"]
 
+    def fetch_game(self, game_id: int):
+        _game = self.fetch(f"games/{game_id}")
+        return CurseGame(
+            id=_game["id"],
+            name=_game["name"],
+            slug=_game["slug"],
+            url=_game["url"],
+            assets=_game["assets"],
+            status=_game["status"],
+            api_status=_game["api_status"],
+            date_modified=_game["date_modified"],
+        )
+
+
 
 @dataclass
 class CurseGame:
