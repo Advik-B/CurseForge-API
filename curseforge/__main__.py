5392cd76a30fb4531548ffca7aa2a3804b762ca7
Advik <advik.b@gmail.com>
2022-11-25 23:21:16 +0530

    Update __main__.py

--- curseforge/__main__.py
+++ curseforge/__main__.py
@@ -1,9 +1,16 @@
 from base64 import b64decode
 from .base import CurseClient
+from json import dump
 from pprint import pprint
 API_KEY: str = b64decode("JDJhJDEwJFhkNkhYT3dweFI1UTIvWGpyZjBkUC5hSDFaRDE5T3pRZC9mVnVNLk94QXJJL01DTlZtNHZh").decode("utf-8")
 
 client = CurseClient(API_KEY)
 
-games = client.fetch("/v1/games")
-pprint(client.fetch("/v1/games"))
+games = client.fetch("games")
+minecraft = next(game for game in games if game["name"] == "Minecraft")
+pprint(minecraft)
+with open("minecraft.json", "w") as f:
+    dump(minecraft, f, indent=4)
+
+# for game in games:
+#     pprint(game["name"]+" : "+str(game["id"]))
