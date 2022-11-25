45ceeaec983391d65a7d3164e6dc7df4ec6b7869
Advik <advik.b@gmail.com>
2022-11-25 22:44:08 +0530

    Update __main__.py

--- curseforge/__main__.py
+++ curseforge/__main__.py
@@ -1,3 +1,5 @@
 from base64 import b64decode
+from .base import CurseClient
 
-API_KEY = b64decode("JDJhJDEwJFhkNkhYT3dweFI1UTIvWGpyZjBkUC5hSDFaRDE5T3pRZC9mVnVNLk94QXJJL01DTlZtNHZh")
+API_KEY: str = b64decode("JDJhJDEwJFhkNkhYT3dweFI1UTIvWGpyZjBkUC5hSDFaRDE5T3pRZC9mVnVNLk94QXJJL01DTlZtNHZh").decode("utf-8")
+client = CurseClient(API_KEY)
