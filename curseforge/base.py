2cc6ceaa514319c1ee46f2d754575dbefb25ead9
Advik <advik.b@gmail.com>
2022-11-25 22:42:06 +0530

    Update base.py

--- curseforge/base.py
+++ curseforge/base.py
@@ -1,3 +1,12 @@
 from requests import get
+from dataclasses import dataclass
 
 BASE_URL = "https://api.curseforge.com"
+
+@dataclass
+class CurseClient:
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+
+    def get(self, url: str):
+        return get(BASE_URL + url, headers={"X-API-Key": self.api_key, "Accept": "application/json"})
