07f8a588f670b1eeaaffd7bb5edb2b0c68dcf6b0
Advik <advik.b@gmail.com>
2022-11-25 22:52:36 +0530

    Update __main__.py

--- curseforge/__main__.py
+++ curseforge/__main__.py
@@ -4,4 +4,4 @@ from .base import CurseClient
 API_KEY: str = b64decode("JDJhJDEwJFhkNkhYT3dweFI1UTIvWGpyZjBkUC5hSDFaRDE5T3pRZC9mVnVNLk94QXJJL01DTlZtNHZh").decode("utf-8")
 
 client = CurseClient(API_KEY)
-print(client.get("/servermods/files?projectIds=1").json())
+print(client.get("/v1/games").json())
