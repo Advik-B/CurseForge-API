325f9c8ada11367b6627d1ac64238f9c8bdb5478
Advik <advik.b@gmail.com>
2022-11-26 00:13:23 +0530

    Update classes.py

--- curseforge/classes.py
+++ curseforge/classes.py
@@ -1,18 +1,33 @@
 from dataclasses import dataclass
 
 @dataclass
-class CurseGameAssets:
+class GameAssets:
     icon_url: str
     tile_url: str
     cover_url: str
 
 @dataclass
-class CurseGame:
+class Game:
     id: int
     name: str
     slug: str
     url: str
-    assets: CurseGameAssets
+    assets: GameAssets
     status: int
     api_status: int
     date_modified: str
+
+
+@dataclass
+class Category:
+    id: int
+    game_id: int
+    name: str
+    slug: str
+    url: str
+    icon_url: str
+    date_modified: str
+    is_Class: bool
+    class_id: int
+    parentCategory_id: int
+    displayIndex: int
