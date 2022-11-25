bba377bce8e5a1a683536f637bef4f214d0146a4
Advik <advik.b@gmail.com>
2022-11-25 23:06:44 +0530

    Update __main__.py

--- curseforge/__main__.py
+++ curseforge/__main__.py
@@ -4,4 +4,4 @@ from pprint import pprint
 API_KEY: str = b64decode("JDJhJDEwJFhkNkhYT3dweFI1UTIvWGpyZjBkUC5hSDFaRDE5T3pRZC9mVnVNLk94QXJJL01DTlZtNHZh").decode("utf-8")
 
 client = CurseClient(API_KEY)
-pprint(client.get("/v1/games").json())
+pprint(client.fetch("/v1/games").json())
