660a451509c9e261498856c5566110ce07d4f0aa
Advik <advik.b@gmail.com>
2022-11-26 23:20:34 +0530

    Update mod.py

--- curseforge/classes/mod.py
+++ curseforge/classes/mod.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from .image import Image
 from .base import CurseObject
+from .category import Category
+from .hashe import Hash
 
 
 @dataclass
@@ -35,7 +37,32 @@ class ModFile(CurseObject):
     file_name: str
     release_type: int
     file_status: int
+    hashes: tuple[Hash]
+    file_date: str
+    file_length: int
+    download_count: int
+    download_url: str
+    game_version: tuple[str]
+
 
 
 @dataclass
-class Mod: pass
+class Mod(CurseObject):
+    id: int
+    game_id: int
+    name: str
+    slug: str
+    links: ModLinks
+    summary: str
+    status: int
+    download_count: int
+    is_featured: bool
+    primary_category_id: int
+    categories: tuple[Category]
+    class_id: int
+    authors: tuple[ModAuthor]
+    logo: ModLogo
+    screenshots: tuple[ScreenShot]
+    mainFile_id: int
+    latestFiles: tuple[ModFile]
+
