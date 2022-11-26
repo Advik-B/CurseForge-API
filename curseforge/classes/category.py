4b6432bde52d82d81cdf31c407ef126b25cd3a4a
Advik <advik.b@gmail.com>
2022-11-26 18:07:38 +0530

    Create category.py

---
+++ curseforge/classes/category.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+@dataclass
+class Category:
+    id: int
+    game_id: int
+    name: str
+    slug: str
+    url: str
+    icon_url: str
+    date_modified: str
+    is_Class: bool
+    class_id: int
+    parentCategory_id: int
+    displayIndex: int
+
