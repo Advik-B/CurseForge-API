e68d71ff0f4d2aa23ceade4c1d721df7bd59059c
Advik <advik.b@gmail.com>
2022-11-26 23:52:42 +0530

    Update json_export.py

--- curseforge/json_export.py
+++ curseforge/json_export.py
@@ -2,20 +2,20 @@ from json import dump, load
 
 from .classes import *
 from .base import CurseClient
-def export_json(curse_obj: CurseObject, file_name: str, indent: int = 4):
+
+
+def export_json(curse_obj: CurseObject) -> dict:
     json = {}
     for key, value in curse_obj.__dict__.items():
         if isinstance(value, CurseObject):
-            json[key] = export_json(value, file_name)
+            json[key] = export_json(value)
         elif isinstance(value, list):
             json[key] = []
             for item in value:
                 if isinstance(item, CurseObject):
-                    json[key].append(export_json(item, file_name))
+                    json[key].append(export_json(item))
                 else:
                     json[key].append(item)
         else:
             json[key] = value
-
-    with open(f"{file_name}.json", "w") as f:
-        dump(json, f, indent=indent)
+    return json
