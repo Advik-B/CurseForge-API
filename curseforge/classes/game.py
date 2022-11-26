c3cd586455a90e5166bdb7f296534adcfe75c5f6
Advik <advik.b@gmail.com>
2022-11-26 18:07:35 +0530

    Create game.py

---
+++ curseforge/classes/game.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+@dataclass
+class Game:
+    id: int
+    name: str
+    slug: str
+    url: str
+    assets: GameAssets
+    status: int
+    api_status: int
+    date_modified: str
+
+@dataclass
+class GameAssets:
+    icon_url: str
+    tile_url: str
+    cover_url: str
