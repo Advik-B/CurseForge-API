6428094ecc5dd449fbeb23096baca0333e712f28
Advik <advik.b@gmail.com>
2022-11-26 23:31:13 +0530

    Update mod.py

--- curseforge/classes/mod.py
+++ curseforge/classes/mod.py
@@ -27,6 +27,30 @@ class ModLogo(Image): pass
 class ScreenShot(Image): pass
 
 
+class SortableGameVersion(CurseObject):
+    game_version_name: str
+    game_version_padded: str
+    game_version: str
+    game_version_release_date: str
+    game_version_type_id: int
+
+
+class Dependency(CurseObject):
+    mod_id: int
+    relation_type: int
+
+class Module(CurseObject):
+    name: str
+    fingerprint: int
+
+class FileIndex(CurseObject):
+    game_version: str
+    file_id: int
+    file_name: str
+    release_type: int
+    game_version_type_id: int
+    mod_loader: int
+
 @dataclass
 class ModFile(CurseObject):
     id: int
@@ -42,7 +66,18 @@ class ModFile(CurseObject):
     file_length: int
     download_count: int
     download_url: str
-    game_version: tuple[str]
+    game_versions: tuple[str]
+    sortable_game_version: tuple[SortableGameVersion]
+    dependencies: tuple[Dependency]
+    expose_as_alternate: bool
+    parent_project_file_id: int
+    altername_file_id: int
+    is_server_pack: bool
+    server_pack_file_id: int
+    file_finger_print: str
+    modules: tuple[Module]
+
+
 
 
 
@@ -65,4 +100,12 @@ class Mod(CurseObject):
     screenshots: tuple[ScreenShot]
     mainFile_id: int
     latestFiles: tuple[ModFile]
+    latestFilesIndexes: tuple[FileIndex]
+    data_created: str
+    data_modified: str
+    data_released: str
+    allow_mod_distribution: bool
+    game_popularity_rank: int
+    is_available: bool
+    thumbs_up_count: int
 
