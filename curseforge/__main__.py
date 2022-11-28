03a6c3a0dad78cd23f32dbcdcc48d4825c49874a
Advik <advik.b@gmail.com>
2022-11-28 14:56:41 +0530

    Update __main__.py

--- curseforge/__main__.py
+++ curseforge/__main__.py
@@ -3,7 +3,7 @@ from .base import CurseClient
 from .classes import Game
 from pprint import pprint
 from requests import get
-from .json_export import export_json, export_dict
+from .export import export_json, export_dict
 API_KEY: str = b64decode("JDJhJDEwJFhkNkhYT3dweFI1UTIvWGpyZjBkUC5hSDFaRDE5T3pRZC9mVnVNLk94QXJJL01DTlZtNHZh").decode("utf-8")
 
 client = CurseClient(API_KEY, cache=True)
