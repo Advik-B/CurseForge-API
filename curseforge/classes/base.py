38fcbe7b2c4f9a9addd682562d75c9fc718bfabb
Advik <advik.b@gmail.com>
2022-11-28 14:18:51 +0530

    Update base.py

--- curseforge/classes/base.py
+++ curseforge/classes/base.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
+from ..json_export import export_json
 
 
 @dataclass
-class CurseObject: pass
+class CurseObject:
+    def to_json(self): return export_json(self)
