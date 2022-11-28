d99f0cce6ee8076d4cd3ed88969edbd9039ca06b
Advik <advik.b@gmail.com>
2022-11-28 14:27:05 +0530

    Update __main__.py

--- curseforge/__main__.py
+++ curseforge/__main__.py
@@ -12,12 +12,4 @@ client = CurseClient(API_KEY, cache=False)
 ID_minecraft: int = 432
 
 minecraft = client.game(ID_minecraft)
-print(minecraft.to_json())
-print("Fetching minecraft")
-response = get("https://api.curseforge.com/v1/games/432", headers={"X-API-Key": API_KEY, "Accept": "application/json"})
-print(response.json())
-
-# for game in client.games():
-#     if game.name == "Minecraft":
-#         minecraft = game
-#         break
+print(minecraft)
