e25fb7d7b338afdb898c0a7463abc9bc68a9b6e2
Advik <advik.b@gmail.com>
2022-12-03 22:24:10 +0530

    Update cursecategory.py

--- curseforge/classes/cursecategory.py
+++ curseforge/classes/cursecategory.py
@@ -15,3 +15,18 @@ class CurseCategory(CurseObject):
     parentCategory_id: int
     displayIndex: int
 
+    @staticmethod
+    def from_dict(data: dict) -> "CurseCategory":
+        return CurseCategory(
+            id=data.get("id"),
+            game_id=data.get("gameId"),
+            name=data.get("name"),
+            slug=data.get("slug"),
+            url=data.get("url"),
+            icon_url=data.get("iconUrl"),
+            date_modified=data.get("dateModified"),
+            is_Class=data.get("isClass"),
+            class_id=data.get("classId"),
+            parentCategory_id=data.get("parentCategoryId"),
+            displayIndex=data.get("displayIndex"),
+        )
