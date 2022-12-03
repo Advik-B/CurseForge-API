4003650cd1ca40fc5ab87067c49d2a0d5f167774
Advik <advik.b@gmail.com>
2022-12-03 21:53:40 +0530

    Create cursecategory.py

---
+++ curseforge/classes/cursecategory.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from .base import CurseObject
+
+@dataclass
+class CurseCategory(CurseObject):
+    id: int
+    game_id: int
+    name: str
+    slug: str
+    url: str
+    icon_url: str
+    date_modified: str
+    is_Class: bool
+    class_id: int
+    parentCategory_id: int
+    displayIndex: int
+
