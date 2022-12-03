bfe0085874f6c8f645ef999f7ccfe14c051ebfff
Advik <advik.b@gmail.com>
2022-12-03 21:53:06 +0530

    Update __main__.py

--- curseforge/__main__.py
+++ curseforge/__main__.py
@@ -1,6 +1,6 @@
 from base64 import b64decode
 from .base import CurseClient
-from .classes import Game
+from .classes import CurseGame
 from pprint import pprint
 from requests import get
 from .export import export_json, export_dict
@@ -8,10 +8,10 @@ API_KEY: str = b64decode("JDJhJDEwJFhkNkhYT3dweFI1UTIvWGpyZjBkUC5hSDFaRDE5T3pRZC
 
 client = CurseClient(API_KEY, cache=True)
 
-# minecraft: Game
+# minecraft: CurseGame
 
 ID_minecraft: int = 432
-minecraft: Game = client.game(ID_minecraft)
+minecraft: CurseGame = client.game(ID_minecraft)
 pprint(minecraft)
 pprint(export_dict(minecraft))
 
