28895f6f9187d33ee65d654c3bf8b11ef675ac98
Advik <advik.b@gmail.com>
2022-12-05 23:05:47 +0530

    Update base.py

--- curseforge/classes/base.py
+++ curseforge/classes/base.py
@@ -1,4 +1,10 @@
 from dataclasses import dataclass
 
 @dataclass
-class CurseObject: pass
+class CurseObject:
+    @property
+    def json(self):
+        for k, v in self.__dict__.items():
+            self.__dict__[k] = v.json if isinstance(v, CurseObject) else v
+        return self.__dict__
+    
