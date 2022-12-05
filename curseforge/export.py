2bd255171933e90c7e40fd02d7f83281fa9d6a7e
Advik <advik.b@gmail.com>
2022-12-05 23:04:30 +0530

    Update export.py

--- curseforge/export.py
+++ curseforge/export.py
@@ -3,13 +3,11 @@ from .classes.base import CurseObject
 
 def export_dict(obj: CurseObject):
     for key, value in obj.__dict__.items():
-        if isinstance(value, CurseObject):
+        if type(obj) == CurseObject:
             obj.__dict__[key] = value.__dict__
     return obj.__dict__
 
 
 def export_json(obj: CurseObject, indent: int = 4):
-    for key, value in obj.__dict__.items():
-        if isinstance(value, CurseObject):
-            obj.__dict__[key] = value.__dict__
-    return dumps(obj.__dict__, indent=indent)
+    dickt = export_dict(obj)
+    return dumps(dickt, indent=indent)
