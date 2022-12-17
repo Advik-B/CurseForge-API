d2255a755439316c9d4515a1761a38aa353a1caf
Advik <advik.b@gmail.com>
2022-12-17 22:21:35 +0530

    Update __main__.py

--- curseforge/__main__.py
+++ curseforge/__main__.py
@@ -5,22 +5,7 @@ API_KEY: str = b64decode("JDJhJDEwJFhkNkhYT3dweFI1UTIvWGpyZjBkUC5hSDFaRDE5T3pRZC
 
 client = CurseClient(API_KEY, cache=True)
 
-mod_file_list = {
-  "minecraft": {
-    "version": "1.18.2",
-    "modLoaders": [
-      {
-        "id": "forge-40.1.84",
-        "primary": True
-      }
-    ]
-  },
-  "manifestType": "minecraftModpack",
-  "manifestVersion": 1,
-  "name": "Sample CMPDL Testing modpack",
-  "version": "6.9.4.2.0",
-  "author": "Advik",
-  "files": [
+mod_file_list = [
     {
       "projectID": 319596,
       "fileID": 3457597,
@@ -46,16 +31,12 @@ mod_file_list = {
       "fileID": 3442690,
       "required": True
     }
-  ],
-  "overrides": "overrides"
-}
+]
 
 # minecraft: CurseGame
 # file = client.get_mod_file(400012, 4083676)
 # print(file.download_url) # https://edge.forgecdn.net/files/4083/676/ExNihiloSequentia-1.18.2-20221113-044349.jar
 
-mods = mod_file_list["files"]
-
-for mod in mods:
+for mod in mod_file_list:
     file = client.get_mod_file(mod["projectID"], mod["fileID"])
     print(file.download_url, flush=True, end="\n\r")
