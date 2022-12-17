c44b5e673664fdb1e388f3aee35926fe3b665554
Advik <advik.b@gmail.com>
2022-12-17 22:23:14 +0530

    Update __main__.py

--- curseforge/__main__.py
+++ curseforge/__main__.py
@@ -1,5 +1,7 @@
 from base64 import b64decode
 from .base import CurseClient
+from sys import stdout
+
 from .classes import CurseGame
 API_KEY: str = b64decode("JDJhJDEwJFhkNkhYT3dweFI1UTIvWGpyZjBkUC5hSDFaRDE5T3pRZC9mVnVNLk94QXJJL01DTlZtNHZh").decode("utf-8")
 
@@ -36,6 +38,8 @@ mod_file_list = [
 # minecraft: CurseGame
 # file = client.get_mod_file(400012, 4083676)
 # print(file.download_url) # https://edge.forgecdn.net/files/4083/676/ExNihiloSequentia-1.18.2-20221113-044349.jar
+
+
 
 for mod in mod_file_list:
     file = client.get_mod_file(mod["projectID"], mod["fileID"])
