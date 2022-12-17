85fe98fb191ce046c722b63c1ff7dd7cf2e22fd1
Advik <advik.b@gmail.com>
2022-12-17 21:35:09 +0530

    Update __main__.py

--- curseforge/__main__.py
+++ curseforge/__main__.py
@@ -6,5 +6,5 @@ API_KEY: str = b64decode("JDJhJDEwJFhkNkhYT3dweFI1UTIvWGpyZjBkUC5hSDFaRDE5T3pRZC
 client = CurseClient(API_KEY, cache=True)
 
 # minecraft: CurseGame
-JEI_file = client.get_mod_file(400012, 4083676)
-print(JEI_file.download_url) # https://edge.forgecdn.net/files/4083/676/ExNihiloSequentia-1.18.2-20221113-044349.jar
+file = client.get_mod_file(400012, 4083676)
+print(file.download_url) # https://edge.forgecdn.net/files/4083/676/ExNihiloSequentia-1.18.2-20221113-044349.jar
