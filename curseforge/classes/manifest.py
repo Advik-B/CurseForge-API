5c4911f2c12e367e4d083db220b5b8deff5cff85
Advik <advik.b@gmail.com>
2023-02-11 13:24:34 +0530

    Update manifest.py

--- curseforge/classes/manifest.py
+++ curseforge/classes/manifest.py
@@ -2,9 +2,17 @@ from dataclasses import dataclass
 from .base import CurseObject
 
 @dataclass
+class CurseFileManifest(CurseObject):
+    project_id: int
+    file_id: int
+    required: bool
+
+@dataclass
 class CurseManifest(CurseObject):
     manifest_type: str
     manifest_version: int
     name: str
     version: str
-
+    author: list[str]
+    files: list[CurseFileManifest]
+    overrides: str
