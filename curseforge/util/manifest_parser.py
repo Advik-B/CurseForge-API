747c1e9d01e6bb1069c8cc1b77ad6b5191a726d6
Advik <advik.b@gmail.com>
2023-02-25 17:57:43 +0530

    Update manifest_parser.py

--- curseforge/util/manifest_parser.py
+++ curseforge/util/manifest_parser.py
@@ -1,6 +1,9 @@
 from json import loads
 from ..classes.manifest import CurseManifest
+from codecs import open
+
 
 def parse_manifest(manifest: str) -> dict:
     """Parses a manifest file and returns a dict"""
-    return loads(manifest)
+    with open(manifest, "r", "utf-8") as file:
+        return CurseManifest.from_dict(loads(file.read()))
