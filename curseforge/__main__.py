737d3ba146173c8279b319c3a100bfc2e74317e1
Advik <advik.b@gmail.com>
2023-02-25 18:31:49 +0530

    Update __main__.py

--- curseforge/__main__.py
+++ curseforge/__main__.py
@@ -1,41 +1,46 @@
 from base64 import b64decode
-from dataclasses import dataclass
-
+from .util import parse_manifest_file
 from .base import CurseClient
+from .classes import CurseManifest
 from sys import stdout
 
 from .classes import CurseGame
-API_KEY: str = b64decode("JDJhJDEwJFhkNkhYT3dweFI1UTIvWGpyZjBkUC5hSDFaRDE5T3pRZC9mVnVNLk94QXJJL01DTlZtNHZh").decode("utf-8")
-    
+
+API_KEY: str = b64decode("JDJhJDEwJFhkNkhYT3dweFI1UTIvWGpyZjBkUC5hSDFaRDE5T3pRZC9mVnVNLk94QXJJL01DTlZtNHZh").decode(
+    "utf-8")
+
 client = CurseClient(API_KEY, cache=True)
 
-mod_file_list = [
+mod_list = [
     {
-      "projectID": 319596,
-      "fileID": 3457597,
-      "required": True
+        "projectID": 319596,
+        "fileID": 3457597,
+        "required": True
     },
     {
-      "projectID": 400012,
-      "fileID": 4083676,
-      "required": True
+        "projectID": 400012,
+        "fileID": 4083676,
+        "required": True
     },
     {
-      "projectID": 314906,
-      "fileID": 3466965,
-      "required": True
+        "projectID": 314906,
+        "fileID": 3466965,
+        "required": True
     },
     {
-      "projectID": 552574,
-      "fileID": 4019567,
-      "required": True
+        "projectID": 552574,
+        "fileID": 4019567,
+        "required": True
     },
     {
-      "projectID": 419699,
-      "fileID": 3442690,
-      "required": True
+        "projectID": 419699,
+        "fileID": 3442690,
+        "required": True
     }
 ]
+
+mod_manifest = parse_manifest_file("manifest.json")
+
 
 # minecraft: CurseGame
 # file = client.get_mod_file(400012, 4083676)
@@ -50,11 +55,7 @@ def Print(*text, end: str = "\n\r", sep: str = " ", flush: bool = False):
         stdout.flush()
 
 
-
-for mod in mod_file_list:
-    file = client.get_mod_file(mod["projectID"], mod["fileID"])
+manifest = parse_manifest_file("manifest.json")
+for mod in manifest.files:
+    file = client.get_mod_file(mod.project_id, mod.file_id)
     Print(file.download_url)
-    # for key in dir(file):
-    #     Print("="*80)
-    #     Print(f"Key: {key}" + " | Value:", eval(f"file.{key}", globals(), locals()))
-    # Print("="*80)
