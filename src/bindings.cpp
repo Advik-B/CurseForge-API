f960fc887cffaca4c235acb3163ae3c7b682518d
Advik <advik.b@gmail.com>
2024-12-06 10:17:07 +0000

    Add addition function to bindings module

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -2,9 +2,11 @@
 
 namespace py = pybind11;
 
+int add(int i, int j) {
+    return i + j;
+}
 
 PYBIND11_MODULE(curseforge, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
-
     m.def("add", &add, "A function which adds two numbers");
 }
