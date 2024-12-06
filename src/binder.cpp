b9106dd69657af8dfdc85912c6b1832e6e47bf2b
Advik <advik.b@gmail.com>
2024-12-06 14:07:58 +0000

    Update CMake configuration and binder module for CurseForge API integration

--- src/binder.cpp
+++ src/binder.cpp
@@ -1,9 +1,13 @@
 #include <pybind11/pybind11.h>
+#include <CurseForgeAPI.hpp>
+
+const auto version = "2.0.0";
 
 namespace py = pybind11;
-const version = "0.1.0";
 
 
 PYBIND11_MODULE(curseforge, m) {
-    m.def("add", [](int i, int j) { return i + j; });
+    m.doc() = "CurseForge API Wrapper";
+    m.attr("__version__") = std::string(version) + "(" + std::string(__VERSION__) + ")";
+
 }
