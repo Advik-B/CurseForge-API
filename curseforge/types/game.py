21ea9ddbb7979d20c56670b106fc5ca391abe1f8
Advik <advik.b@gmail.com>
2024-12-07 16:00:43 +0000

    Add static methods to CurseGame for creating instances from dict and fetching games

--- curseforge/types/game.py
+++ curseforge/types/game.py
@@ -12,3 +12,24 @@ class CurseGame:
     assets: CurseAsset
     status: int
     apiStatus: int
+
+
+    @staticmethod
+    def from_dict(data: dict):
+        return CurseGame(
+            id=data["id"],
+            name=data["name"],
+            slug=data["slug"],
+            dateModified=datetime.fromisoformat(data["dateModified"]),
+            assets=CurseAsset.from_dict(data["assets"]),
+            status=data["status"],
+            apiStatus=data["apiStatus"]
+        )
+
+    @staticmethod
+    def list(api: CurseForgeAPI):
+        return [CurseGame.from_dict(game) for game in api.fetch("/v1/games")]
+
+    @staticmethod
+    def from_id(game_id: int, api: CurseForgeAPI):
+        return CurseGame.from_dict(api.fetch(f"/v1/games/{game_id}"))
