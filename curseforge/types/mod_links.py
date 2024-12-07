70305643b9e35841a95a992153d16465187bb96b
Advik <advik.b@gmail.com>
2024-12-07 17:58:42 +0000

    Fix return type in from_dict method to use CurseModLinks instead of ModLinks

--- curseforge/types/mod_links.py
+++ curseforge/types/mod_links.py
@@ -9,7 +9,7 @@ class CurseModLinks:
 
     @staticmethod
     def from_dict(d: dict):
-        return ModLinks(
+        return CurseModLinks(
             websiteUrl=d.get('websiteUrl'),
             wikiUrl=d.get('wikiUrl'),
             issuesUrl=d.get('issuesUrl'),
