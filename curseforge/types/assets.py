0a25bb9bc4440fd91c46d7de6ea42e65185142c5
Advik <advik.b@gmail.com>
2024-12-07 16:17:23 +0000

    Add static method to CurseAssets for instance creation from dictionary

--- curseforge/types/assets.py
+++ curseforge/types/assets.py
@@ -5,3 +5,11 @@ class CurseAssets:
     iconUrl: str
     tileUrl: str
     coverUrl: str
+
+    @staticmethod
+    def from_dict(data: dict):
+        return CurseAssets(
+            iconUrl=data["iconUrl"],
+            tileUrl=data["tileUrl"],
+            coverUrl=data["coverUrl"]
+        )
