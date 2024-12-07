ae0907cc91a4bfc7cb3e700c312b22336fc679dd
Advik <advik.b@gmail.com>
2024-12-07 16:41:55 +0000

    Fetch specific CurseGame instance by ID in test.py

--- test.py
+++ test.py
@@ -6,6 +6,4 @@ from curseforge.types import CurseGame
 API = CurseForgeAPI(os.getenv("CURSEFORGE_TOKEN"), use_cache=False)
 
 games = CurseGame.s(API) # The .s() method for any type in curseforge that will list all of *it* from the API
-
-for game in games:
-    print(game.id, game.name)
+minecraft = CurseGame.from_id(432, API) # The .from_id() method for any type in curseforge that will fetch a specific one from the API
