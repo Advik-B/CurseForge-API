3e670c118ee5fc411f1672b3dd561c2e7200b7e7
Advik <advik.b@gmail.com>
2024-12-07 16:22:00 +0000

    Update CurseGame method call from 'list' to 's' for brevity in test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from curseforge.types import CurseGame
 
 API = CurseForgeAPI(os.getenv("CURSEFORGE_TOKEN"), use_cache=False)
 
-games = CurseGame.list(API)
+games = CurseGame.s(API) # The .s() method for any type in curseforge that will list all of *it* from the API
 
 for game in games:
     print(game.id, game.name)
